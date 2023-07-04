3f20ec8447635f020e9fd6a16c6efd6a4e245276
PabloVGA <pablo.villa@Solera.com>
2023-07-04 16:59:50 +0200

    User controller creation

---
+++ banking-app/src/main/java/com/solera/bankingapp/UserController.java
@@ -0,0 +1,42 @@
+package com.solera.bankingapp;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import com.solera.bankingapp.models.User;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserController {
+	private DataManager dataM;
+	public UserController () {
+		
+		dataM = new DataManager();
+	}
+	
+	@GetMapping("/users/{id}")
+	public String listUsers (@PathVariable String id)
+	{
+		return dataM.getOne(id).toString();
+	}
+	@GetMapping("/users")
+	public String listUsers ()
+	{
+		return dataM.getAll().toString();
+	}
+	@PostMapping("/users/create")
+	public ResponseEntity<Object> createUser(@RequestBody User MyUser){
+		User userCreated = dataM.saveOne(MyUser);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userCreated.getUsername()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	
+}
